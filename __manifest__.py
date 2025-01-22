c34b77de58922aecda6c64c9fef0062371a92a4c
Julio de León <jalj153@outlook.com>
2025-01-22 05:37:11 -0600

    20250122 0536 | 00.00.065 Carga datos con xml para el primer archivo.

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': '(CN006) Gestión Proyectos IT',
-    'version': '00.00.064',
+    'version': '00.00.065',
     'summary': '(CN006) Gestión Proyectos IT',
     'description': '(CN006) Gestión de proyectos del área de IT',
     'author': 'Neotropo®',
@@ -35,7 +35,7 @@
                 'views/cn006_menu.xml',
 
         # datos / cron
-            # 'data/cn006_proyecto_clasificacion.csv',
+             'data/cn006_proyecto_clasificacion.xml',
             # 'data/cn006_proyecto_grado_avance.csv',
             # 'data/cn006_proyecto_grado_complejidad.csv',
             # 'data/cn006_proyecto_nivel_importancia.csv',
