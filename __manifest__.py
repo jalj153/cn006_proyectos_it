00bb0437fb77bdd918aeda90ffeb95eca36d0547
Julio de León <jalj153@outlook.com>
2025-01-27 11:21:01 -0600

    20250127 1120 | 00.00.108 Presenta pantalla para crear proyectos, es necesario que el acceso sea diferente.

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': '(CN006) Gestión Proyectos IT',
-    'version': '00.00.099',
+    'version': '00.00.108',
     'summary': '(CN006) Gestión Proyectos IT',
     'description': '(CN006) Gestión de proyectos del área de IT',
     'author': 'Neotropo®',
@@ -16,20 +16,15 @@
     'icon': '/cn006_proyectos_it/static/description/icon.png',
     'depends': ['base','project','hr_timesheet'],
     'data': [
-        # Código especial que no necesita referencia en el manifest
-
-
         # Definición de la seguridad
             'security/cn006_security.xml',
             'security/ir.model.access.csv',
         
+            #'views/cn006_project_project_view_form_simplified.xml',
+            'views/cn006_project_project_view_form.xml',
             'views/cn006_menu_actions_special.xml',
         # Orden Menú:  Vistas > Acciones > Menú
             # Vistas
-                #'views/.xml',
-                
-                'views/cn006_project_project_view_form_simplified.xml',
-                'views/cn006_project_project_view_form.xml',
                 'views/cn006_project_project_view_kanban_mod.xml',
                 'views/cn006_project_task_view_form.xml',
                 'views/cn006_proyecto_clasificacion_views.xml',
@@ -38,7 +33,6 @@
                 'views/zzzcn006_menu_actions_dummy.xml',
             # Menú
                 'views/cn006_menu.xml',
-
         # datos / cron
             'data/cn006_proyecto_clasificacion.xml',
             'data/cn006_proyecto_grado_avance.xml',
