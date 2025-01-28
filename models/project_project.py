657a36f86e2e31194f571375408dd42ff3ca8152
Julio de León <jalj153@outlook.com>
2025-01-27 22:02:23 -0600

    20250127 2200 | 00.00.138 Crea etapas tarea CN006.  Presesnta kanba de proyectos y lleva a tareas

--- models/project_project.py
+++ models/project_project.py
@@ -5,8 +5,8 @@ class ProjectProject(models.Model):
 
     #region Campos relacionados
     cn006_clasificacion_id     = fields.Many2one('cn006.proyecto.clasificacion',     string='Clasificación', help='Clasificación del proyecto según la categoría definida.')
-    cn006_grado_avance_id      = fields.Many2one('cn006.proyecto.grado.avance',      string='Avance', help='Grado de avance del proyecto.')
-    cn006_grado_complejidad_id = fields.Many2one('cn006.proyecto.grado.complejidad', string='Compejidad', help='Grado de complejidad del proyecto.')
+    cn006_grado_avance_id      = fields.Many2one('cn006.proyecto.grado.avance',      string='Grado Avance', help='Grado de avance del proyecto.')
+    cn006_grado_complejidad_id = fields.Many2one('cn006.proyecto.grado.complejidad', string='Grado Compejidad', help='Grado de complejidad del proyecto.')
     cn006_nivel_importancia_id = fields.Many2one('cn006.proyecto.nivel.importancia', string='Importancia', help='Nivel de importancia del proyecto.')
     cn006_nivel_urgencia_id    = fields.Many2one('cn006.proyecto.nivel.urgencia',    string='Urgencia', help='Nivel de urgencia del proyecto.')
     cn006_tamano_id            = fields.Many2one('cn006.proyecto.tamano',            string='Tamaño', help='Tamaño del proyecto.')
