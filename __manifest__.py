593e3a5858153033d046751f9cd1df4a9f18c890
Julio de León <jalj153@outlook.com>
2025-01-27 23:54:26 -0600

    20250127 1150 | 00.00.148 Backup general

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': '(CN006) Gestión Proyectos IT',
-    'version': '00.00.138',
+    'version': '00.00.148',
     'summary': '(CN006) Gestión Proyectos IT',
     'description': '(CN006) Gestión de proyectos del área de IT',
     'author': 'Neotropo®',
@@ -25,12 +25,17 @@
             'views/cn006_menu_actions_special.xml',
         # Orden Menú:  Vistas > Acciones > Menú
             # Vistas
-                
-                'views/cn006_project_task_view_form.xml',
-                'views/cn006_proyecto_stage_tree.xml',
-                'views/cn006_proyecto_clasificacion_views.xml',
                 'views/cn006_project_project_view_kanban_filter.xml',
-                'views/cn006_project_project_view_kanban_mod.xml',
+                'views/cn006_project_project_view_kanban_tasks.xml',
+
+                'views/cn006_project_task_view_form.xml',
+                'views/cn006_project_task_view_kanban_filter.xml',
+                'views/cn006_project_task_view_kanban.xml',
+
+                'views/cn006_proyecto_clasificacion_views.xml',
+                'views/cn006_proyecto_stage_tree.xml',
+                
+                
             # Acciones
                 'views/cn006_menu_actions.xml',
                 'views/zzzcn006_menu_actions_dummy.xml',
