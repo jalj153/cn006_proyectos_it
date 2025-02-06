184bfcbd7de095f633cba0846a7ab2fddee96742
Julio de León <jalj153@outlook.com>
2025-02-05 18:12:44 -0600

    20250205 1810 | 00.00.216 Reestructuración de carpetas para separar los scripts de consola - NO HAY CAMBIO EN FUNCIONALIDAD

--- z_scripts_consola_varios/kpis_previo_actualizacion/cn006_kpis.py
+++ z_scripts_consola_varios/kpis_previo_actualizacion/cn006_kpis.py
@@ -1,3 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.abspath(".."))  # Agrega el directorio superior al path
+
+
 from cn006_kpis_globales import cCN006_globales
 import xlsxwriter
 import io
